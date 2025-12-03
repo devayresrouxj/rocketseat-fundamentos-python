256152498fd5519f019a93af086f0904fdc9320d
Ayres Roux <devayresrouxj@gmail.com>
2025-12-03 13:51:05 -0300

    aula: numeros

---
+++ tipos_varaveis/numeros.py
@@ -0,0 +1,40 @@
+# Inteiro
+numero_inteiro = 3
+print("Inteiro =", numero_inteiro)
+
+# Real com ponto flutuante
+numero_real = 3.14
+print("Real/Float =", numero_real)
+
+# type() -> Retorna o tipo de uma variável ou valor
+print("Tipo da variável inteiro =", type(numero_inteiro))
+print("Tipo da variável real =", type(numero_real))
+
+# Soma +
+soma = 1 + 1
+print("1 + 1 =", soma)
+
+# Subtração -
+subtracao = 1 - 1
+print("1 - 1 =", subtracao)
+
+# Multiplicação *
+multiplicacao = 2 * 2
+print("2 x 2 =", multiplicacao)
+
+# Divisão /
+divisao = 5 / 2
+print("5 / 2 =", divisao)
+print("Tipo da variável do resultado da divisão =", type(divisao))
+
+# Divisão Inteiro //
+divisao_inteiro = 5 // 2
+print("5 // 2 =", divisao_inteiro)
+
+# Conversão de tipos
+print("Valor em inteiro =", int(divisao))
+print("Valor em float =", float(soma))
+
+# Módulo % -> Resto da divisão
+modulo = 5 % 2
+print("5 % 2 =", modulo)
