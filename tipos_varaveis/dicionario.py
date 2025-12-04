75bc872cf9391ec86511ca853ebed7911fd6586c
Ayres Roux <devayresrouxj@gmail.com>
2025-12-04 07:08:56 -0300

    aula: dicionários

---
+++ tipos_varaveis/dicionario.py
@@ -0,0 +1,12 @@
+# Declaração
+pessoa = {"nome": "João", "idade": 30, "cidade": "São Paulo"}
+print("Meu dicionário de exemplo:", pessoa)
+
+# Acessando propriedades
+print("Nome:", pessoa["nome"])
+print("Idade:", pessoa["idade"])
+print("Cidade:", pessoa["cidade"])
+
+# Criando nova propriedade
+pessoa["sobrenome"] = "Silva"
+print("Meu dicionário modificado:", pessoa)
