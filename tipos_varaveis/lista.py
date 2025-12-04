6ca70052ec43f6264481748b56590829427c7ff4
Ayres Roux <devayresrouxj@gmail.com>
2025-12-04 06:36:15 -0300

    aula: listas

---
+++ tipos_varaveis/lista.py
@@ -0,0 +1,13 @@
+# Declaração
+minha_lista = [1, 2, 3, 4, 5, "rocketseat", True, False]
+
+print("Minha lista de exemplo: ", minha_lista)
+
+# Exibindo elementos da lista
+print("minha_lista[0]:", minha_lista[0])
+print("minha_lista[5]:", minha_lista[5])
+
+# Selecionando o intervalo de uma lista (fatiamento)
+print("minha_lista[1:7]:", minha_lista[1:7])
+print("minha_lista[:6]:", minha_lista[:6])
+print("minha_lista[3:]:", minha_lista[3:])
