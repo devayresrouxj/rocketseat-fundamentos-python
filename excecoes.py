d9a7dae32b69b27a95013913ba74c75e4b2d3066
Ayres Roux <devayresrouxj@gmail.com>
2025-12-04 08:27:50 -0300

    aula: exceções

---
+++ excecoes.py
@@ -0,0 +1,12 @@
+try:
+  numero = int(input("Digite um número inteiro: "))
+  resultado = 10 / numero
+except ValueError as e:
+  print(f"Erro de value error: {e}")
+  raise ValueError("Tipo de variáveis incompatíveis")
+except Exception as e:
+  print(f"Erro: {e}")
+else:
+  print("Resultado:", resultado)
+finally:
+  print("Operação finalizada")
