948f3dbd7ccf5d5b7eb3b4448e03215c79c4bfb3
Ayres Roux <devayresrouxj@gmail.com>
2025-12-04 07:49:04 -0300

    aula: continuação de loops for

--- for.py
+++ for.py
@@ -24,3 +24,18 @@ for valor in pessoa.values():
 print("Chave e Valor:")
 for chave, valor in pessoa.items():
   print(f"{chave} = {valor}")
+
+# range() -> Retorna um intervalo numérico em formato de lista
+for numero in range(5):
+  print("Número:", numero)
+
+# len() -> Retorna a quantidade de itens na lista
+lista = [1, 2, 3, 4, 5]
+for indice in range(0, len(lista)):
+  print("Índice:", indice)
+  print("Elemento:", lista[indice])
+
+# enumerate() -> 
+lista_enumarate = ['a', 'b', 'c']
+for indice, valor in enumerate(lista_enumarate, start=1):
+  print(f"{indice} - {valor}")
